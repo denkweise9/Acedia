25e63e22b162820017ff8e2a99b88d5c261b747f
Scott <Lvl4Sword@users.noreply.github.com>
2016-03-28 22:47:25 -0600

    Update store.py

--- sloth/store.py
+++ sloth/store.py
@@ -1,3 +1,5 @@
+# Copyright 2015, 2016 Scott King
+#
 # This file is part of Sloth.
 #
 # Sloth is free software: you can redistribute it and/or modify
@@ -12,6 +14,7 @@
 #
 # You should have received a copy of the Affero GNU General Public License
 # along with Sloth.  If not, see <http://www.gnu.org/licenses/>.
+#
 import os
 import json
 
