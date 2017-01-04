fe11abdebb32a384ba87c541c8fc6e6d3f1c5b43
denkweise9 <denkweise9@gmail.com>
2017-01-04 14:12:52 -0500

    Please add to RPG_Elements.py
    We need more information to help us figure out the big picture.

---
+++ sloth/RPG_Elements.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+#RPG_Elements.py     Contains all the elements (XP, Currency, Characters, etc) of the RPG
+
+import sloth
+
+
+#General NPC Monsters in game. Not for quest/storyline.  Contains currency_dropped, Xp_given, and items_dropped if any.
+
+RPG_Elements_Monsters = {
+"Monster":{"stuff", 20, 20},
+
+}
+
+# Races in game
+RPG_Elements_Races = {
+"Humans",
+"Elves",
+"Dwarves",
+"Goblins",
+"Skeletons"
+}
+
+# Characters that assit the user 
+RPG_Elements_Characters = {
+
+
+}
+
+
