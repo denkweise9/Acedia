146cc903769407ec794cbc79782143b33d00cc33
denkweise9 <denkweise9@gmail.com>
2017-01-04 13:48:40 -0500

    RPG_Elements.py

---
+++ sloth/RPG_Elements.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+import sloth
+
+#Races in Game
+RPG_Elements_Races = {
+"Elves",
+"Humans",
+"Dwarves",
+"Goblins",
+"Skeletons"
+}
+
+#Characters that are teamed with our user
+RPG_Elements_Characters = {
+
+
+}
+
+
+#NPC's to kill. Just General NPC's not quest/story related ones.
+#Would contain Currency_Dropped, Items Dropped, and XP_Given.
+RPG_Elements_Monsters = {
+
+
+}
