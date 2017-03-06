6333f3ac4416b81eef8edf8d7b51cbbc33ec33d8
denkweise9 <denkweise9@users.noreply.github.com>
2017-03-06 03:01:00 -0500

    Update RPG_Elements.py

--- sloth/RPG_Elements.py
+++ sloth/RPG_Elements.py
@@ -23,15 +23,22 @@ RPG_Elements_Characters = {
 
 #NPC's to kill. Just General NPC's not quest/story related ones.
 #Would contain Currency_Dropped, Items Dropped, and XP_Given.
-RPG_Elements_Monsters = {
+RPG_Elements_NPCs = {
     Goblin_reg,
-    Cow
+    Goblin_general,
+    Skeleton,
+    Cow,
+    Pig,
+    Horse,
+    Chicken,
+    Goat,
+   
 
 }
 
 
 
-#below is an experiment
+#below is an experiment, is not final, and needs updating
 class Goblin_reg(object):
     agility = 3
     #charisma and intelligence are not applied because this is a regular npc goblin
@@ -45,9 +52,35 @@ class Goblin_reg(object):
             if taget.life <= 0:
                 print("target dead")
                 return False
+            
+class Goblin_general(object):
+    agility = 3
+    
+    def __init__(self, life, attack_lvl, defence_lvl):  
+        self.life = life
+        self.attack_lvl = attack_lvl
+        self.defence_lvl = defence_lvl
+    def attack(self, target):
+        if attack_lvl >= target.defence_lvl:
+            target.life -= 10
+            if taget.life <= 0:
+                print("target dead")
+                return False
               
-              
-              
+class Goat(object):
+     
+     def __init__(self, life, attack_lvl, defence_lvl):
+          self.life = life
+          self.attack_lvl = attack_lvl
+          self.defence_lvl = defence_lvl
+     def attack(self, target):
+         if attack_lvl >= target.defence_lvl:
+             target.life -= 10
+             if taget.life <= 0:
+                 print("target dead")
+                 return False
+          
+          
 #The following below is for exmaples only
 c = Goblin_reg(10,10,10)
 g = Goblin_reg(10,10,10)
